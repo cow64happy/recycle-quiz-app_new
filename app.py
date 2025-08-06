4118f3c861887ad0c880e281c314049e2798ea0e
cow64happy <122846453+cow64happy@users.noreply.github.com>
2025-08-06 10:03:54 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="재활용 지도 앱", page_icon="🌍", layout="wide")
+
+st.title("♻️ 2023년 분리배출량 지도 보기")
+
+st.markdown(
+    '[🗺️ 2023년 지도 보기](https://cow64happy.github.io/recycle-quiz-app/recycle_map_2023.html)',
+    unsafe_allow_html=True
+)
